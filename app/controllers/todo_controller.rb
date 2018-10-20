9b5f4990fe80d072b600f287f64f698544875717
Khloe Wright <khloewright329@gmail.com>
2018-10-20 19:23:11 +0000

    pushrubyyy

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,6 +3,20 @@ class TodoController < ApplicationController
 #erb = embedded ruby
     def index #ouraction
         
-        
+    end
+    def show #ouraction
+        todo_id = params[:id]
+        if todo_id == '1'
+            @todo_description = "clean room"
+        end
+        if todo_id == '2'
+            @todo_description = "apush homework"
+        end
+        if todo_id == '3'
+            @todo_description = "sweep floor"
+        end
+        if todo_id == '4'
+            @todo_description = "do laundry"
+        end
     end
 end
