c1f0b35445712d56d9380dfaf573ad1329434921
Khloe Wright <khloewright329@gmail.com>
2018-10-21 19:09:50 +0000

    fastpushsad

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -5,22 +5,50 @@ class TodoController < ApplicationController
         
     end
     def show #ouraction
-        todo_id = params[:id]
-        if todo_id == '1'
-            @todo_description = "Clean Room"
-            @todo_time = "1 hour"
-        end
-        if todo_id == '2'
-            @todo_description = "Apush Homework"
-            @todo_time = "3 hours"
-        end
-        if todo_id == '3'
-            @todo_description = "Sweep Floor"
-            @todo_time = "30 mins"
-        end
-        if todo_id == '4'
-            @todo_description = "Do Laundry"
-            @todo_time = "1 hour"
-        end
+       
+        #get the row with that id
+        @todo = Todo.find_by_id(params[:id])
+        # if todo_id == '1'
+        #     @todo_description = "Clean Room"
+        #     @todo_time = "1 hour"
+        # end
+        # if todo_id == '2'
+        #     @todo_description = "Apush Homework"
+        #     @todo_time = "3 hours"
+        # end
+        # if todo_id == '3'
+        #     @todo_description = "Sweep Floor"
+        #     @todo_time = "30 mins"
+        # end
+        # if todo_id == '4'
+        #     @todo_description = "Do Laundry"
+        #     @todo_time = "1 hour"
+        # end
+    end
+   
+    def new
+        
+    end
+ 
+    def create
+        t = Todo.new
+        t.description = params['description']
+        t.added_date = DateTime.now
+        #saves to the database
+        t.save
+        
+        redirect_to "/todo/show/#{t.id}"
+    end
+    
+    def edit
+        @todo = Todo.find_by_id(params[:id])
+    end
+    
+    def update
+        t = Todo.find_by_id(params[:id])
+        t.description = params['description']
+        t.save
+        
+        redirect_to "/todo/show/#{t.id}"
     end
 end
